8d11f072cf3d1d0ca541ff58f176278cc97ed2a5
pipohost <mahmood.amash@gmail.com>
2025-12-01 21:44:57 +0200

    Updated main page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
-  return <h1coming soon</h1>;
+// app/page.js
+
+// هذا هو أبسط مكون لصفحة Next.js
+export default function Home() {
+  return (
+    <div>
+      {/* هذا هو المحتوى المصدري الذي سيظهر */}
+      <h1>تم النشر بنجاح يا محمود!</h1>
+    </div>
+  );
 }
