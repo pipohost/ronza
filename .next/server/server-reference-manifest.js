c86f8ecd07b5585fd60dcb492c37401885115759
pipohost <mahmood.amash@gmail.com>
2025-12-02 00:54:13 +0200

    Fixed Tailwind CSS syntax error in globals.css

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{\"4eec3469c34a018e2561905d128c7800cbba10f9\":{\"workers\":{\"app/page\":\"819\"},\"layer\":{\"app/page\":\"rsc\"}}},\"edge\":{},\"encryptionKey\":\"LvLq8c9HNmHM27yY0myZ/T23JXijvB3t0lMX+i3uH/0=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{\"4eec3469c34a018e2561905d128c7800cbba10f9\":{\"workers\":{\"app/page\":\"819\"},\"layer\":{\"app/page\":\"rsc\"}}},\"edge\":{},\"encryptionKey\":\"YH+dpqwxRQetjsmkqbZMZK+Y6YSnZ/c9mT7I2C7ovRc=\"}"
